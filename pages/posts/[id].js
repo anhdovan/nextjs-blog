396ed3e4fe9d78e5b339621588404d2a8c4a4244
anhdovan <dvanh1974@yahoo.com>
2023-05-25 20:45:19 +0700

    first commit

---
+++ pages/posts/[id].js
@@ -0,0 +1,38 @@
+
+import { getAllPostIds, getPostData } from '../../lib/posts';
+
+export async function getStaticProps({ params }) {
+    // Add the "await" keyword like this:
+    const postData = await getPostData(params.id);
+
+    return {
+        props: {
+            postData,
+        },
+    };
+}
+
+import Layout from '../../components/layout';
+
+export async function getStaticPaths() {
+    const paths = getAllPostIds();
+    return {
+        paths,
+        fallback: false,
+    };
+
+}
+
+export default function Post({ postData }) {
+    return (
+        <Layout>
+            {postData.title}
+            <br />
+            {postData.id}
+            <br />
+            {postData.date}
+            <br />
+            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        </Layout>
+    );
+}
